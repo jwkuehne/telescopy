7ed3aaab8cc749a98ca0209fe98f733803cc2c7d
jwkuehne <105461718+jwkuehne@users.noreply.github.com>
2022-05-12 21:49:55 -0500

    Delete ost65 directory

--- ost65/_CATALOGS/NGCcull.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-# Cull non-Dreyer lines
-while ($line = <>) {
-   ($N, $CAT, $STAR, @REST) = split("	", $line);
-   if ($STAR eq '*') {
-      print "$line";
-   }
-   $STAR = '';
-}
